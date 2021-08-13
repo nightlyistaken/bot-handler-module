555a727a890cd7919644c4dc74e97d0b5cf37cee
dhairy-online <dhairysrivastava5@gmail.com>
2021-08-13 21:15:35 +0530

    fix: `flush` :panda:

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
-use tts_rust::text_speech;
 use tts_rust::text_speech_text;
 use std::io;
+use std::io::Write;
+
 /// Prompts on the CLI
 pub fn readline(msg: &str) -> String {
-    println!("{}", msg);
-
+    print!("{}", msg);
+    std::io::stdout().flush().unwrap();
     let mut user_input = String::new();
+    
     io::stdin().read_line(&mut user_input).unwrap();
 
     user_input
+    
 }
 
-/// This will prompt
-pub fn if_speech_out(prefix :&str ,input: &str, output: &str) {
+/// Prompt manager: 
+pub fn if_msg(prefix :&str ,input: &str, output: &str) {
     let inside = readline(&prefix );
     if inside.contains(&input) {
         text_speech_text(&output);
@@ -22,6 +25,13 @@ pub fn if_speech_out(prefix :&str ,input: &str, output: &str) {
 
 #[test]
 fn test() {
-    if_speech_out("___","YO", "hi");
-    if_speech_out("-------","HEY!", "yo lol");
+    //' -  ' = This is will indicate the input is being typed at this line
+    //' Hello, World! ' = Message to be typed by user
+    //' Hey! I'm a bot! ' = Output of the message typed by user
+    // Output will be --
+
+    // - Hello, World!
+    //   Hey! I'm a bot!
+    
+    if_msg("- ","Hello, World!", "Hey! I'm a bot!");
 }
