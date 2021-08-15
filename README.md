51bd06a35b8b8257195c9176cab8914ff4d6a642
dhairy-online <dhairysrivastava5@gmail.com>
2021-08-15 21:48:18 +0530

    version `0.1.3` :banana:

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### `Command and chat handler for rust`
+## Command and chat handler for bots
 
 #### User-friendly bot-handler module for rust
 
@@ -16,31 +16,53 @@
 Add this to your cargo.toml file:
 
 ```toml
-   bot-handler-manager = "0.1.2"
+   bot-handler-manager = "0.1.3"
 ```
 
 #### main.rs
 
 ```rust
-// use
+// Use the crate
 use bot_handler_manager;
 
 fn main() {
-     let mut commander = bot_handler_manager::Commander::new("You~> ");
-     commander.on("hi", "Hello there!");
-     // Loop and read
+    // Make a prefix
+     let mut commander = bot_handler_manager::Commander::new("~> ");
+
+    // Listen for messages/commands
+     commander.on("Hello!", |_| {"Hello there!"}.to_string());
+
+    // loop the readline()
      loop {
-         commander.read();
+         // read 
+         commander.read(Some(|input: &str| {
+             // if the user types a message/command, and it contains in the vector,
+             // the user can type in MIX-CASE.
+
+            //  example: "HeLLO, RuST!"
+
+            let my_user_input = vec!["hello, rust!", "hello, world!", "hello, bot!", "hi!"];
+
+            if my_user_input.contains(&input) {
+                String::from("Hello, I'm Alive!")
+             } else {
+                 String::from(" ")
+             }
+         }));
      }
 }
 
 
 ```
 
-Feel free to open issues and contribute!
+### `Feel free to open issues and contribute!`
 
-#### breadA#3012
+### Authors
 
-### This repo is licensed under:
+- `breadA#3012`
+
+### This crate and repo is licensed under:
 
 - MIT
+
+Thanks! :smiley:
--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,78 @@
-
+//! Welcome to bot-handler-managing module!
+//! This module is User-friendly, Fell free to use!
+// `                                              `
+//! See the Docs for documentation
 
 #![allow(dead_code)]
 use tts_rust::text_speech_text;
 use std::collections::HashMap;
 use std::io::Write;
 use std::io;
+use std::boxed::Box;
+
+/// Make a Struct commander with prefix and handler(s).
 pub struct Commander {
     prefix: String,
-    handlers: HashMap<String, String>,
+    handlers: HashMap<String, Box<dyn Fn(String) -> String>>,
 }
 
  impl Commander {
+     /// Creates a new prefix (string)
     pub fn new(prefix: &str) -> Commander {
         Commander {
             prefix: prefix.to_string(),
             handlers: HashMap::new(),
         }
     }
-
-    pub fn on(&mut self, key: &str, value: &str) {
-        self.handlers.insert(key.to_string(), value.to_string());
+    /// Creates a new input and output for the command/message
+    pub fn on(&mut self, key: &str, handler: fn(String) -> String) {
+        self.handlers.insert(key.to_string(), Box::new(handler));
     }
 
-    pub fn read(&self) {
-        let input = readline(&self.prefix);
+    /// Read those "commander.on()" messages/commands...
+    pub fn read(&self, global_handler: Option<fn(&str) -> String>) {
+        let input = readline(&self.prefix).to_lowercase();
         let input = input.trim();
         if self.handlers.contains_key(input) {
             let answer = self.handlers.get(input).unwrap();
-            text_speech_text(answer);
+            text_speech_text(&answer(input.to_string()));
+        } else {
+            match global_handler {
+                Some(h) => {
+                    text_speech_text(&h(input));
+                },
+                None => {}
+            }
         }
     }
 }
 
-/// Prompts on the CLI
+/// Prompts on the Command line, like in JS you "prompt()".
 pub fn readline(msg: &str) -> String {
     print!("{}", msg);
     std::io::stdout().flush().unwrap();
     let mut user_input = String::new();
     io::stdin().read_line(&mut user_input).unwrap();
-    user_input
+    return user_input;
     
 }
 
 #[test]
 fn test() {
-     
+   
      text_speech_text("hello!");
      let mut commander = Commander::new("~> ");
-     commander.on("hi", "Hello");
+     commander.on("hi", |_| {"Hello"}.to_string());
      // Commands section
      loop {
-         commander.read();
+         commander.read(Some(|input: &str| {
+            let my_user_input = vec!["hello, rust!", "hello, world!", "hello, bot!", "hi!"];
+            if my_user_input.contains(&input) {
+                String::from("Pudu")
+             } else {
+                 String::from(" ")
+             }
+         }));
      }
 }
 
